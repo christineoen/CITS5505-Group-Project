0511d49098cd2699a7a0cfcb63dbf008188861c8
Christine Oen <oen.christine@gmail.com>
2026-03-19 10:09:29 +0800

    remove files

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-in-production")
-    DATABASE = os.path.join(os.path.dirname(__file__), "instance", "database.db")
-    DEBUG = True
